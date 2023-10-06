e1bcb5c09b64f84ed66501c448aba6354d1ee8a0
SarahElizabeteTDS <130590547+SarahElizabeteTDS@users.noreply.github.com>
2023-10-06 14:26:22 -0300

    Create safadometro_tela.php

---
+++ safadometro_tela.php
@@ -0,0 +1,19 @@
+<h1>Safadômetro</h1>
+
+<form>
+
+    <input type="text" placeholder="Informe o dia... ">
+    <br/>
+    <br/>
+    <input type="text" placeholder="Informe o mês...">
+    <br/>
+    <br/>
+    <input type="text" placeholder="Informe o ano...">
+    <br/>
+    <br/>
+
+    <input type="submit" value="calcular">
+
+</form>
+
+<img src="https://br.web.img2.acsta.net/c_310_420/pictures/17/06/22/21/16/082721.jpg">
